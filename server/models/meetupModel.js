ca680a7e6102b66f23b030a48db75b7d81052d5a
rjiang12 <roy.jiang.4757@gmail.com>
2022-07-19 11:58:19 -0700

    postgres link

--- server/models/meetupModel.js
+++ server/models/meetupModel.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 
-const PG_URI = 'postgres://mclhgmni:A_0e62HiTHMmNkgLY3s9oMeLAecSP_j3@rajje.db.elephantsql.com/mclhgmni';
+const PG_URI = 'postgres://rcwvxixj:QoMsDTMRQyrIxkI_ZfWPvUvHwdifXLt4@heffalump.db.elephantsql.com/rcwvxixj';
 
 const pool = new Pool({
   connectionString: PG_URI
