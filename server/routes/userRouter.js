ccd2fdafbcb7964d6722475d2c985c6bb1bde1b6
irnatus <stanchenw@gmail.com>
2022-07-20 20:48:30 -0400

    fixed minor bugs

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,6 +3,8 @@ const userController = require('../controllers/userController.js');
 const cookieController = require('../controllers/cookieController.js');
 const userRouter = express.Router();
 
+const path = require ('path');
+
 
 userRouter.post('/signup', 
     userController.createUser, 
@@ -16,6 +18,9 @@ userRouter.post('/login',
         return res.status(200).send(); // return ssid instead
 });
 
+userRouter.get('/*', (req, res) => {
+        return res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
+      });    
 module.exports = userRouter;
 
 
